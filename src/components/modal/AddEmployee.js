5e02e24056a0608e73cf354ca198bdfcdeefb46d
Nazif-Malhi <nazifmalhi@gmail.com>
2022-12-01 17:58:26 +0500

    employee-phone number

--- src/components/modal/AddEmployee.js
+++ src/components/modal/AddEmployee.js
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { TextField, MenuItem, FormControl, Select } from "@mui/material";
+import React, { useEffect, useState,useMemo, forwardRef } from "react";
+import { TextField, MenuItem, FormControl, Select, OutlinedInput, InputLabel } from "@mui/material";
 import { Row, Col, Modal, Container, Button, Spinner } from "react-bootstrap";
+import countryList from "react-select-country-list";
+import ReactPhoneInput from 'react-phone-input-material-ui';
+
 
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -11,16 +14,23 @@ import {
   get_Designation,
   getGroups,
   register,
-  updateUserData,
-  add_employee,
-  user_reg_clearErrors,
   update_employee,
   get_Employees,
 } from "../../store";
 import { isNumber, is_emailValid } from "../../utils";
+import styled from "styled-components";
+
+const CustomInput = styled(ReactPhoneInput)`
+input{
+  padding:8.5px 14px;
+}
+`;
+
 
 function AddEmployee({ onHandleCallBack, ...props }) {
   const dispatch = useDispatch();
+  const options = useMemo(() => countryList().getData(), []);
+
   const { groups, group_error } = useSelector((state) => state.groups);
   const { designations, designation_error } = useSelector(
     (state) => state.designations
@@ -31,15 +41,38 @@ function AddEmployee({ onHandleCallBack, ...props }) {
   const { user_reg, user_reg_error, is_reg } = useSelector(
     (state) => state.user_reg
   );
-  const {update_emp, update_emp_error, is_updated_emp} = useSelector( (state) => state.update_emp)
+  const {update_emp, update_emp_error, is_updated_emp} = useSelector( (state) => state.update_emp);
+  const [employee_id, setEmployeeId] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [date, setDate] = useState("");
-  const [department_input, setDepartment_Input] = useState("");
+
+  const [user_name, setUser_name] = useState("");
+  const [role_title, setRole_title] = useState("");
   const [email, setEmail] = useState("");
+
+  const [department_input, setDepartment_Input] = useState("");
   const [designation, setDesignation] = useState("");
   const [group, setGroup] = useState("");
+
+  const[country, setCountry] = useState("");
   const [phone, setPhone] = useState("");
+  const [package_title, setPackage_title] = useState("");
+
+  const[city, setCity] = useState("");
+  const[province, setProvince] =  useState("");
+  const [address, setAddress] = useState("");
+
+  const [companyName, setCompanyName] = useState("");
+  const [companyType, setCompanyType] = useState("");
+
+  const [industry, setIndustry] = useState("");
+  const [annual, setAnnual] = useState("");
+
+  const [totalHeadCount, setTotalHeadCount] = useState("");
+  const [marketShare, setMarketShare] = useState("");
+  const [type, setType] = useState("");
+
+
   const [email_validation_error, setEmail_validation_error] = useState(false);
   const [spinner_trigger, setSpinner_trigger] = useState(false);
 
@@ -215,7 +248,19 @@ function AddEmployee({ onHandleCallBack, ...props }) {
       <Modal.Body className="show-grid">
         <Container>
           <Row style={{ marginBottom: "10px" }}>
-            <Col xs={8} md={6}>
+          <Col >
+              <TextField
+                id="outlined-name"
+                label="Employee ID"
+                size="small"
+                value={employee_id}
+                onChange={(e) => {
+                  setEmployeeId(e.target.value);
+                }}
+                fullWidth
+              />
+            </Col>
+            <Col>
               <TextField
                 id="outlined-name"
                 label="First Name"
@@ -227,7 +272,7 @@ function AddEmployee({ onHandleCallBack, ...props }) {
                 fullWidth
               />
             </Col>
-            <Col xs={8} md={6}>
+            <Col >
               <TextField
                 id="outlined-name"
                 label="Last Name"
@@ -242,7 +287,32 @@ function AddEmployee({ onHandleCallBack, ...props }) {
           </Row>
 
           <Row style={{ marginBottom: "10px" }}>
-            <Col xs={8} md={6}>
+          <Col >
+              <TextField
+                id="outlined-name"
+                label="User Name"
+                size="small"
+                fullWidth
+                value={user_name}
+                onChange={(e) => {
+                  setUser_name(e.target.value);
+                }}
+              />
+            </Col>
+            <Col >
+              <TextField
+                id="outlined-name"
+                label="Role Title"
+                size="small"
+                fullWidth
+                value={role_title}
+                onChange={(e) => {
+                  setRole_title(e.target.value);
+                }}
+              />
+            </Col>
+            
+            <Col>
               <TextField
                 id="outlined-name"
                 label="Email"
@@ -254,7 +324,11 @@ function AddEmployee({ onHandleCallBack, ...props }) {
                 }}
               />
             </Col>
-            <Col xs={8} md={6}>
+            
+          </Row>
+          <Row style={{ marginBottom: "10px" }}>
+            
+            <Col >
               <FormControl
                 sx={{ width: "100%" }}
                 size="small"
@@ -281,22 +355,7 @@ function AddEmployee({ onHandleCallBack, ...props }) {
                 </Select>
               </FormControl>
             </Col>
-          </Row>
-          <Row style={{ marginBottom: "10px" }}>
-            <Col xs={8} md={6}>
-              <TextField
-                type={"text"}
-                id="outlined-name"
-                label="Phone Number"
-                size="small"
-                fullWidth
-                value={phone}
-                onChange={(e) => {
-                  setPhone(isNumber(e.target.value));
-                }}
-              />
-            </Col>
-            <Col xs={8} md={6}>
+            <Col >
               <FormControl
                 sx={{ width: "100%" }}
                 size="small"
@@ -323,9 +382,7 @@ function AddEmployee({ onHandleCallBack, ...props }) {
                 </Select>
               </FormControl>
             </Col>
-          </Row>
-          <Row style={{ marginBottom: "10px" }}>
-            <Col xs={8} md={6}>
+            <Col>
               <FormControl
                 sx={{ width: "100%" }}
                 size="small"
@@ -353,6 +410,205 @@ function AddEmployee({ onHandleCallBack, ...props }) {
               </FormControl>
             </Col>
           </Row>
+          <Row style={{ marginBottom: "10px" }}>
+          <Col>
+                  <FormControl
+                    sx={{ width: "100%" }}
+                    size="small"
+                    style={{ background: "white" }}
+                  >
+                    <InputLabel id="demo-multiple-name-label">
+                      Country{" "}
+                    </InputLabel>
+                    <Select
+                      labelId="demo-multiple-name-label"
+                      id="demo-multiple-name"
+                      value={country.length > 0 ? country : ""}
+                      onChange={(e) => {setCountry(e.target.value); console.log(e.target.value)}}
+                      input={<OutlinedInput label="Name" />}
+                    >
+                      {options.map((val, id) => {
+                        return (
+                          <MenuItem key={val.value} value={val.value}>
+                            {val.label}
+                          </MenuItem>
+                        );
+                      })}
+                    </Select>
+                  </FormControl>
+                </Col>
+                <Col >
+              {/* <TextField
+                type={"text"}
+                id="outlined-name"
+                label="Phone Number"
+                size="small"
+                fullWidth
+                value={phone}
+                onChange={(e) => {
+                  setPhone(isNumber(e.target.value));
+                }}
+              /> */}
+              <CustomInput
+              component={TextField}
+              countryCodeEditable = {false}
+              country={country.toLowerCase() || 'us'}
+      value={phone}
+      onChange={setPhone} />
+            </Col>
+            <Col >
+              <TextField
+                type={"text"}
+                id="outlined-name"
+                label="Pakage Title"
+                size="small"
+                fullWidth
+                value={package_title}
+                onChange={(e) => {
+                  setPackage_title(e.target.value);
+                }}
+              />
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: "10px" }}>
+          <Col >
+              <TextField
+                type={"text"}
+                id="outlined-name"
+                label="City"
+                size="small"
+                fullWidth
+                value={city}
+                onChange={(e) => {
+                  setCity(e.target.value);
+                }}
+              />
+            </Col>
+            <Col >
+              <TextField
+                type={"text"}
+                id="outlined-name"
+                label="Province"
+                size="small"
+                fullWidth
+                value={province}
+                onChange={(e) => {
+                  setProvince(e.target.value);
+                }}
+              />
+            </Col>
+            <Col >
+              <TextField
+                type={"text"}
+                id="outlined-name"
+                label="Address"
+                size="small"
+                fullWidth
+                value={address}
+                onChange={(e) => {
+                  setAddress(e.target.value);
+                }}
+              />
+            </Col>
+          </Row>
+          <Row style={{ marginBottom: "10px" }}>
+          <Col>
+                  <TextField
+                    id="outlined-name"
+                    label="Company Name"
+                    size="small"
+                    fullWidth
+                    value={companyName}
+                    onChange={(e) => {
+                      setCompanyName(e.target.value);
+                    }}
+                  />
+                </Col>
+                <Col>
+                  <TextField
+                type={"text"}
+                id="outlined-name"
+                label="Company Type"
+                size="small"
+                fullWidth
+                value={companyType}
+                onChange={(e) => {
+                  setCompanyType(e.target.value);
+                }}
+              />
+                </Col>
+                <Col>
+                <TextField
+                type={"text"}
+                id="outlined-name"
+                label="Industry"
+                size="small"
+                fullWidth
+                value={industry}
+                onChange={(e) => {
+                  setIndustry(e.target.value);
+                }}
+              />
+                </Col>
+          </Row>
+          <Row style={{ marginBottom: "10px" }}>
+          <Col>
+          
+                  <TextField
+                type={"text"}
+                id="outlined-name"
+                label="Annual Revenue in USD"
+                size="small"
+                fullWidth
+                value={annual}
+                onChange={(e) => {
+                  setAnnual(e.target.value);
+                }}
+              />
+                </Col>
+                <Col>
+                <TextField
+                type={"text"}
+                id="outlined-name"
+                label="Total Head Count"
+                size="small"
+                fullWidth
+                value={totalHeadCount}
+                onChange={(e) => {
+                  setTotalHeadCount(e.target.value);
+                }}
+              />
+                </Col>
+                <Col>
+                <TextField
+                type={"text"}
+                id="outlined-name"
+                label="Market Share"
+                size="small"
+                fullWidth
+                value={marketShare}
+                onChange={(e) => {
+                  setMarketShare(e.target.value);
+                }}
+              />
+                </Col>
+          </Row>
+          <Row style={{ marginBottom: "10px" }}>
+          <Col xs={6} md={4}>
+              <TextField
+                type={"text"}
+                id="outlined-name"
+                label="Type"
+                size="small"
+                fullWidth
+                value={type}
+                onChange={(e) => {
+                  setType(e.target.value);
+                }}
+              />
+            </Col>
+          </Row>
+
           <p
             style={{
               color: "red",
